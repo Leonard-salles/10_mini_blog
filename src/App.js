81baca9b92c8a3a7a3235b03ab494f9fd3da3220
Leonardo Salles <lsleonardosalles.999@gmail.com>
2024-07-14 22:17:12 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+
+import './App.css';
+
+
+import { BrowserRouter, Routes, Route, navigate, Navigate} from "react-router-dom"
+import { onAuthStateChanged } from 'firebase/auth';
+
+//hooks
+
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
+// contexts
+
+import { AuthProvider } from './Context/AuthContext';
+
+// pages
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import NavBar from "./components/Nav/NavBar"
+import Footer from "./components/Footer/Footer"
+import Register from "./pages/Register/Register"
+import Login from "./pages/Login/Login"
+import { CreatePost } from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
+import { EditPost } from './pages/EditPost/EditPost';
+
+function App() {
+  const [user, setUser] = useState(undefined)
+
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
+  return (
+    <div className="App">
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <NavBar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/search' element={<Search />} />
+              <Route path='/posts/:id' element={<Post />} />
+              <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
+              <Route path='/login' element={!user ? <Login /> : <Navigate to="/" /> } />
+              <Route path='/posts/edit/:id' element={user ? <EditPost /> : <Navigate to="/login" /> } />
+              <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" /> } />
+              <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" /> } />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
